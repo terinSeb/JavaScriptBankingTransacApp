0ca0132a97117cf59c8a8c984e89ccd97a930852
Terin Sebastin <terin01@gmail.com>
2022-03-19 06:01:31 +0530

    Poplutaing deposits and withdrawal

--- script.js
+++ script.js
@@ -128,6 +128,20 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // const currenSet = new Set([
 //   'USD','EUR','GBP','EUR','GBP'
 // ])
-currenSet.forEach(function(curr,i,map){
-  console.log(`${curr} .... ${i}`)
-})
+// currenSet.forEach(function(curr,i,map){
+//   console.log(`${curr} .... ${i}`)
+// })
+const displayMovements = function(movements){
+  containerMovements.innerHTML = ''
+  movements.forEach(function(mov,i){
+    const type = mov > 0 ? 'deposit' : 'withdrawal'
+const html = `
+<div class="movements__row">
+<div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+<div class="movements__value">${mov}</div>
+</div>
+`
+containerMovements.insertAdjacentHTML('afterbegin',html)
+    })
+}
+displayMovements(account1.movements);
